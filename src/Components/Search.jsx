cff116d842984802ca12ba694697539cae6f0d57
jddubbya <jdworam@gmail.com>
2024-08-07 15:52:56 -0500

    Update Search.jsx

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,17 +1,30 @@
+/*
+*  Search.jsx
+*
+*  Purpose: To take the first and last names entered on the page and
+*           pass them to the server to query the database 
+*  Exports: Search - the method used to make the database query.
+*  HTML:    Builds the page used to enter first and last name with a "Search" button
+*/
 import Results from "./Results";
 import { useDispatch } from "react-redux";
 import {setVoterData} from "../../Redux/slice.cjs";
 
 const Search = ({firstName, setFirstName, lastName, setLastName, data, setData}) => {
+  
   const dispatch = useDispatch();
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    const response = await fetch(`/db/get/?firstName=${firstName}&lastName=${lastName}`);
+    const response = await fetch(`/db/getVoters/?firstName=${firstName}&lastName=${lastName}`);
     const result = await response.json();
+    if(!result) {
+      return;
+    }
     setData(result);
     dispatch(setVoterData(result));
   }
+
   return (
   <>
   <form onSubmit={submitHandler}>
@@ -26,11 +39,14 @@ const Search = ({firstName, setFirstName, lastName, setLastName, data, setData})
     value={lastName}>
     </input>
     <p> </p>
-    <input type="submit" value="Search"></input>
+    <input className="searchButton" type="submit" value="Search"></input>
   </form>
   <Results data={data} setData={setData}/>
   </>
   )
+}
+
+export default Search;
 }
 
 export default Search;
