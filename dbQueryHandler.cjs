b415477832768769e3e9996f4b65cfa1d00ab1e5
jddubbya <jdworam@gmail.com>
2024-08-07 15:36:23 -0500

    Update dbQueryHandler.cjs

--- dbQueryHandler.cjs
+++ dbQueryHandler.cjs
@@ -1,3 +1,11 @@
+/*
+*  dbQueryHandler.cjs
+*
+*  Purpose: 1. To execute queries against the database
+*           2. To listen on port 3000
+*  
+*/
+
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
@@ -5,8 +13,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const pool = require('./dbConnectionPool.cjs');  // JDW
-
-console.log("ConnectionManager.cjs loaded..........");
 
 // __dirname is an environment variable that tells you the absolute path 
 // of the directory containing the currently executing file.
@@ -19,12 +25,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 })
 
+//**************************************************************
 // Method to get voter data based on voter's first and last name.
 
-app.get("/db/get", (req, res) => {
+app.get("/db/getVoters", (req, res) => {
+  const vstate = "texas";
+  const county = "rockwall";
   const { firstName, lastName } = req.query;
-  let sql = 'SELECT * FROM voterlookup2 WHERE name like "' + firstName + '%' + lastName + '"';
-  console.log("SQL = " . sql)
+  let sql = 'SELECT * FROM ' + vstate + '_' + county +  
+  ' WHERE FIRST_NAME = "' + firstName + '" AND LAST_NAME = "' + lastName + 
+  '" ORDER BY LAST_NAME, FIRST_NAME';
+  console.log(sql);
   pool.query(sql, (err, results) => {
     if (err) {
       console.error("Error executing query: " + err.stack);
@@ -34,6 +45,35 @@ app.get("/db/get", (req, res) => {
     res.json(results);
   });
 });
+
+//**************************************************************
+// Method to get list of states.
+
+app.get("/db/getStates", (req, res) => {
+  let sql = 'SELECT DISTINCT STATE FROM STATE_COUNTY ORDER BY STATE ASC';
+  pool.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).send("Error fetching users");
+      return;
+    }
+    res.json(results);
+  });
+});
+
+//**************************************************************
+// Method to get list of counties based on the State selected.
+
+app.get("/db/getCounties", (req, res) => {
+   const { stateName } = req.query;
+   let sql = 'SELECT COUNTY FROM STATE_COUNTY WHERE STATE = "' + stateName + '"';
+   pool.query(sql, (err, results) => {
+     if (err) {
+       res.status(500).send("Error fetching users");
+       return;
+     }
+     res.json(results);
+   });
+ });
 
 // Listen on Server Port 3000
 
