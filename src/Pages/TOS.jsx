e670e5cdb4c02364a16804da20f308ffb2e5edd5
jddubbya <jdworam@gmail.com>
2024-08-07 21:10:46 -0500

    Update TOS.jsx

--- src/Pages/TOS.jsx
+++ src/Pages/TOS.jsx
@@ -1,23 +1,19 @@
 const TOS = () => {
-return(
-  <>
-    <p>Before You Get Started
-VoteRef.com provides on-demand 
-public access to voter registration 
-records collected from state and local 
-election officials.
-Our mission is to ensure fair elections by 
-providing voters with full transparency and 
-easy access to voting records, which historically 
-have been difficult to obtain.
-VoteRef.com is for election-related, noncommercial 
-use and for users based in the United States only. 
-Before accessing the site, please read our Terms of 
-Service and agree you will abide by them by clicking 
-on the Accept button below.
-    </p>
-  </>
-)
+    return(
+      <>
+      <h2>
+        Terms of Service
+      </h2>
+      <body>
+        <object class="pdf" 
+            data=
+            "../../Content/VoCheckTOS.pdf"
+            width="800"
+            height="800">
+        </object>
+      </body>
+      </>
+  )
 }
-
+    
 export default TOS
