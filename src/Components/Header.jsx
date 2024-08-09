98530e1ee5921c396ff3e71ea9c33cee926f86fc
jddubbya <jdworam@gmail.com>
2024-08-09 11:03:46 -0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
-import logo from '../../Content/vocheck.jpeg'
+import logo from "../../Content/vocheck.jpeg";
 
 const Header = () => {
-    return (
-      <>
-        <header>
-          <div className="vocheckHeaderContainer">
-            <p id="topHeader"> VoCheck Voter Lookup</p>
-          </div>
-        </header>
+  return (
+    <>
+      <header>
+        <div className="vocheckHeaderContainer">
+          <p id="topHeader"> VoCheck Voter Lookup</p>
+        </div>
         <section className="logoSection">
-        <Link to="/">
-            <img src={logo} width={60} height={60}/>
-        </Link>
-        <h1 className="titleH1">VoCheck</h1>
+          <Link to="/">
+            <img src={logo} width={60} height={60} />
+          </Link>
+          <h1 className="titleH1">VoCheck</h1>
         </section>
-        
-      </>
-    );
-  };
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
