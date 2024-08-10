d32160e9dcccd5079cc4d997bae26365ae17e004
jddubbya <jdworam@gmail.com>
2024-08-10 11:54:08 -0500

    added more data to the page

--- src/Pages/SingleResult.jsx
+++ src/Pages/SingleResult.jsx
@@ -22,14 +22,21 @@ const SingleResult = ({ data }) => {
             return (
               <>
                 <section className="singleDataSec">
-                  <h2>{voter.NAME}</h2>
+                  <h3>{voter.NAME}</h3>
+                  <h4><span class="tab"></span>Age: {voter.AGE}</h4>
+                  <h4><span class="tab"></span>Party: {voter.PARTY}</h4>
+                  <h4><span class="tab"></span>Address: {voter.ADDRESS}</h4>
+                  <h4><span class="tab"></span>Municipality: {voter.MUNICIPALITY}</h4>
+                  <h4><span class="tab"></span>Registration Date: {voter.REG_DATE}</h4>
+                  <h4><span class="tab"></span>Voter Status: {voter.VOTER_STATUS}</h4>
                   <br></br>
-                  <h3>Registration Date: {voter.REG_DATE}</h3>
-                  <h3>Voter Status: {voter.VOTER_STATUS}</h3>
-                  <h3>Address: {voter.ADDRESS}</h3>
-                  <h3>Precinct: {voter.PRECINCT}</h3>
-                  <h3>Age: {voter.AGE}</h3>
-                  <h3>Party: {voter.PARTY}</h3>
+                  <h3>Political Districts:</h3>
+                  <h4><span class="tab"></span>Precinct: {voter.PRECINCT}</h4>  
+                  <h4><span class="tab"></span>Congressional District: {voter.CONGR_DIST}</h4>
+                  <h4><span class="tab"></span>State Senate District: {voter.STATE_SEN_DIST}</h4>
+                  <h4><span class="tab"></span>State House District: {voter.STATE_HOUSE_DIST}</h4>
+                  <h4><span class="tab"></span>County Commissioner District: {voter.CNTY_COMMISS_DIST}</h4>
+                  <h4><span class="tab"></span>School District: {voter.SCHOOL_DIST}</h4>
                 </section>
                 <br></br>
                 <Link to="/">
