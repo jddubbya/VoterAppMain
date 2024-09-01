9e07d19dd69a73bfce33809cc3528534d04e0ee8
jddubbya <jdworam@gmail.com>
2024-09-01 16:56:18 -0500

    Update index.cjs

--- Routes/index.cjs
+++ Routes/index.cjs
@@ -20,7 +20,7 @@ app.use("/db", dataRoute);
 
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '../dist/index.html'));
   });
 
 app.listen(PORT, () => {
