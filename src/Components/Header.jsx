49dcc95e43029ac952fe09eeb0e3e2b49ba789fd
jddubbya <jdworam@gmail.com>
2024-09-01 16:15:41 -0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,57 @@
 import { Link } from "react-router-dom";
 import logo from "../../Content/vocheck.jpeg";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setToken } from "../../Redux/authSlice.cjs";
 
 const Header = () => {
+  const navigate = useNavigate();
+  let token = useSelector(state => state.authorization.token);
+  const dispatch = useDispatch();
+
+const reload = () => {
+  window.location.reload();
+}
+
+  const logoutHandler = () => {
+    dispatch(setToken(''));
+    sessionStorage.removeItem("token");
+    navigate('/');
+    reload();
+};
+
+if(sessionStorage.getItem('token')){
+  token = sessionStorage.getItem('token')
+};
+
+
+
   return (
     <>
       <header>
         <div className="vocheckHeaderContainer">
-          <h4 id="topHeader"> VoCheck Voter Lookup</h4>
+          <div className="emptyBox"></div>
+          <section className="headTextCont">
+            <h4 id="topHeader"> VoCheck Voter Lookup</h4>
+          </section>
+          <section className="loginButtonCont">
+            {token ? 
+            <>
+            <select className="adminDrop">
+              <option>Admin Options:</option>
+              <option>Manage Users</option>
+            </select>
+            <button className="logInOutButton" onClick={logoutHandler}>Log Out</button>
+            </>
+            : 
+            <Link to="/login" style={{ textDecoration: 'none' }}><input
+            className="logInOutButton"
+            type="submit"
+            value="Login"
+          ></input></Link>
+            }
+          </section>
         </div>
         <section className="logoSection">
           <Link to="/">
@@ -19,4 +64,4 @@ const Header = () => {
   );
 };
 
-export default Header
+export default Header;
