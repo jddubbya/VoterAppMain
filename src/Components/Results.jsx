718c5c7a7f1acbdef98517a46b6ad475434c2a88
jddubbya <jdworam@gmail.com>
2024-09-01 16:16:22 -0500

    Update Results.jsx

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -10,20 +10,29 @@ import { useSelector } from "react-redux";
 
 const Results = () => {
   const currentSearch = useSelector((state) => state.voterData.voterData);
+  const numMatches = currentSearch.length;
+  let matchText = "Matches:"
+  if (numMatches === 1) {
+    matchText = "Match:"
+  }
   return (
     <>
+      <section className="searchMatchLabel">
+        <p> {numMatches} {matchText}</p>
+      </section>
       <section className="resultsSection">
-      {!currentSearch.length ? null : (
-        <ul>
-          {currentSearch.map((voter) => {
-            return (
-              <li className="resultItem" key={voter.SOS_VOTERID}>
-                <Link to={`/${voter.SOS_VOTERID}`}>{voter.NAME}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+        {!currentSearch.length ? null : (
+          <ul>
+            {currentSearch.map((voter) => {
+              return (
+                <li className="resultItem" key={voter.SOS_VOTERID}>
+                  <Link to={`/${voter.SOS_VOTERID}`}>{voter.NAME}</Link>
+                </li>
+
+              );
+            })}
+          </ul>
+        )}
       </section>
     </>
   );
