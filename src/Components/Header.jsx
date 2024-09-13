12cc748698d98f4f3b1800b23ea4c57ac72550cc
jddubbya <jdworam@gmail.com>
2024-09-12 20:26:06 -0500

    clean up header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,47 +10,27 @@ const Header = () => {
   let token = useSelector(state => state.authorization.token);
   const dispatch = useDispatch();
 
-const reload = () => {
-  window.location.reload();
-}
+  const reload = () => {
+    window.location.reload();
+  }
 
   const logoutHandler = () => {
     dispatch(setToken(''));
     sessionStorage.removeItem("token");
     navigate('/');
     reload();
-};
+  };
 
-if(sessionStorage.getItem('token')){
-  token = sessionStorage.getItem('token')
-};
-
-
+  if (sessionStorage.getItem('token')) {
+    token = sessionStorage.getItem('token')
+  };
 
   return (
     <>
       <header>
-        <div className="vocheckHeaderContainer">
-          <div className="emptyBox"></div>
-          <section className="headTextCont">
+        <div className="topHeaderContainer">
+          <section>
             <h4 id="topHeader"> VoCheck Voter Lookup</h4>
-          </section>
-          <section className="loginButtonCont">
-            {token ? 
-            <>
-            <select className="adminDrop">
-              <option>Admin Options:</option>
-              <option>Manage Users</option>
-            </select>
-            <button className="logInOutButton" onClick={logoutHandler}>Log Out</button>
-            </>
-            : 
-            <Link to="/login" style={{ textDecoration: 'none' }}><input
-            className="logInOutButton"
-            type="submit"
-            value="Login"
-          ></input></Link>
-            }
           </section>
         </div>
         <section className="logoSection">
