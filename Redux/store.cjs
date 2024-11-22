a11095d3d9a46a0fcb1fbd129fa9c2c6dc295e90
jddubbya <jdworam@gmail.com>
2024-11-22 13:55:21 -0600

    Update store.cjs

--- Redux/store.cjs
+++ Redux/store.cjs
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
 import voterDataSlice from './slice.cjs';
-import authSlice from './authSlice.cjs';
 
 export default configureStore ({
   reducer: {
-    voterData: voterDataSlice,
-    authorization: authSlice
+    voterData: voterDataSlice
   }
 });
