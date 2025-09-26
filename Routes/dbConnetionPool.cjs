c296680a6348dbde7d31f7e9aa83672700d05967
jddubbya <jdworam@gmail.com>
2025-09-26 16:04:17 -0500

    SWITCH TO aws

--- Routes/dbConnetionPool.cjs
+++ Routes/dbConnetionPool.cjs
@@ -7,13 +7,15 @@
 
 var mysql = require('mysql')
 
-// Database connection parameters
-var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: '107.180.114.231',
-    user: 'vocheck_user',
-    password: 'Your217With#',
-    database: 'voterdata'
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'rpm-master.ckt226mgqu6e.us-east-1.rds.amazonaws.com',
+    user: 'rpm_root',
+    password: 'Hissy621Loot#',
+    database: 'vocheck',
+    waitForConnections: true,
+    port: 3306,
+    queueLimit: 0
 });
 
 // Create a pool of connections - number specified above by
