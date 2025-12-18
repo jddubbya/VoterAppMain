2e6d41d9190bcaa570dc9eca169f449ebc991f52
jddubbya <jdworam@gmail.com>
2025-12-18 15:34:38 -0600

    Update dbConnetionPool.cjs

--- Routes/dbConnetionPool.cjs
+++ Routes/dbConnetionPool.cjs
@@ -5,35 +5,26 @@
 * Description: Creates and manages a pool of database connections
 */
 
-var mysql = require('mysql')
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'rpm-master.ckt226mgqu6e.us-east-1.rds.amazonaws.com',
-    user: 'rpm_root',
-    password: 'Hissy621Loot#',
+    host: 'electionstech.ckt226mgqu6e.us-east-1.rds.amazonaws.com',
+    user: 'electionstech',
+    password: 'Jess176Hiss!',
     database: 'vocheck',
     waitForConnections: true,
     port: 3306,
     queueLimit: 0
 });
 
-// Create a pool of connections - number specified above by
-// "connectionLimit"
-
-pool.getConnection((err, connection) => {
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.');
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.');
-        }
+(async () => {
+    try {
+        const connection = await pool.getConnection();
+        connection.release();
+    } catch (err) {
+        console.error("dbConnectionPool error:", err);
     }
-    if (connection) connection.release()
-    return;
-})
+})();
+
 module.exports = pool;
