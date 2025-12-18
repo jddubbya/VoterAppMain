63b142aa62cc9ec45bd57608934236088a528b30
jddubbya <jdworam@gmail.com>
2025-12-18 15:37:56 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    proxy: { 
-      "/db" : "http://localhost:3000",
-      secure: false,   // added by JDW
-    }}
+  server: {
+    proxy: {
+      '/db': {
+         target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
