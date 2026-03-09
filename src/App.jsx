84ca00631ed1ac292e21bcbc7bb29813273f0fae
jddubbya <jdworam@gmail.com>
2026-03-09 16:20:10 -0500

    Refactor App component to include authentication

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@
 // React
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Login PIN imports
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
+
 // VoCheck
 import './App.css';
 import Home from './Pages/Home';
@@ -21,25 +26,40 @@ import TOS from './Pages/TOS';
 import Privacy from './Pages/Privacy';
 import Maps from './Pages/Maps';
 import Charts from './Pages/Charts';
+import LoginPage from './Pages/LoginPage';
 
 
 function App() {
   const [selectedOption, setSelectedOption] = useState(
     localStorage.getItem('selectedOption') || '');
   return (
-    <>
-      <section className="bodyCont">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home selectedOption={selectedOption} setSelectedOption={setSelectedOption} />}></Route>
-          <Route path="/tos" element={<TOS />}></Route>
-          <Route path="/privacy" element={<Privacy />}></Route>
-          <Route path="/maps" element={<Maps selectedOption={selectedOption} />}></Route>
-          <Route path="/charts" element={<Charts selectedOption={selectedOption} />}></Route>
-        </Routes>
-        <Footer className="footerCont"/>
-      </section>
-    </>
+
+    <AuthProvider>
+      <Routes>
+        {/* PUBLIC ROUTE */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* PROTECTED APP */}
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <section className="bodyCont">
+                <Header />
+                <Routes>
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/tos" element={<TOS />} />
+                  <Route path="/privacy" element={<Privacy />} />
+                  <Route path="/maps" element={<Maps />} />
+                  <Route path="/charts" element={<Charts />} />
+                </Routes>
+                <Footer className="footerCont" />
+              </section>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   )
 };
 
